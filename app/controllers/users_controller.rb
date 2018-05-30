56977bdf6bade0e984038dbd3103b6893c99d85e
Thanh Binh <thankBinh995@github.com>
2018-05-30 11:10:54 +0700

    Chapter 11: Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,78 @@
 class UsersController < ApplicationController
-  def index; end
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :find_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.ordered_by_name.paginate page: params[:page],
+      per_page: Settings.num_page
+  end
 
   def new
     @user = User.new
   end
 
-  def show
+  def show; end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = t ".flash_mail_check"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".flash_update_sc"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".flash_update_delete"
+      redirect_to users_url
+    else
+      render :index
+      flash[:danger] = t ".flash_destroy_fail"
+    end
+  end
+
+  private
+
+  def find_user
     @user = User.find_by id: params[:id]
     return if @user
     flash[:danger] = t "static_pages.home.err_find_user"
     redirect_to users_path
   end
 
-  def create
-    @user = User.new user_params
-    if @user.save
-      log_in @user
-      flash[:success] = t "static_pages.home.home_h1"
-      redirect_to @user
-    else
-      render :new
-    end
-  end
-
-  private
-
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    flash.now[:danger] = t ".flash_update_dg"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
