c643c5680c0e412e80eca4663ff0fe0f8b112305
Thanh Binh <thankBinh995@github.com>
2018-06-03 16:28:45 +0700

    Chapter 12: Password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  before_save{email.downcase!}
-  validates :name,  presence: true, length: {maximum: Settings.length_max.name}
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+  validates :name, presence: true, length: {maximum: Settings.length_max.name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum:
     Settings.length_max.email},
@@ -13,12 +15,9 @@ class User < ApplicationRecord
   scope :ordered_by_name, ->{order(name: :asc)}
 
   def self.digest string
-    if ActiveModel::SecurePassword.min_cost
-      cost = BCrypt::Engine.MIN_COST
-    else
-      cost = BCrypt::Engine.cost
-    end
-    BCrypt::Password.create string, cost: cost
+    @cost = BCrypt::Engine.MIN_COST if ActiveModel::SecurePassword.min_cost
+    @cost = BCrypt::Engine.cost
+    BCrypt::Password.create string, cost: @cost
   end
 
   def self.new_token
@@ -37,5 +36,39 @@ class User < ApplicationRecord
 
   def forget
     update_attribute :remember_digest, nil
+  end
+
+  def activate
+    update_attribute :activated, true
+    update_attribute :activated_at, Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute :reset_digest,  User.digest(reset_token)
+    update_attribute :reset_sent_at, Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get "password_resets/new"
+  get "password_resets/edit"
   get "sessions/new"
   get "users/new"
   get "users/index"
@@ -13,5 +15,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users
+  resources :account_activations, only: :edit
+  resources :password_resets, only: %i(new create edit update)
 
 end
